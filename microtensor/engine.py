d54cb97ea0434710e31727d6280ba506a9592967
nibir-ai <nibir.github@gmail.com>
2026-03-03 18:48:32 +0530

    feat: implement complete mathematical operator suite (sub, div, pow, exp)

--- microtensor/engine.py
+++ microtensor/engine.py
@@ -1,25 +1,22 @@
+import math
+
 class Value:
     """
     A scalar value that tracks its computational history for automatic differentiation.
     """
     def __init__(self, data, _children=(), _op='', label=''):
         self.data = data
-        self.grad = 0.0 
-        self._backward = lambda: None 
-        self._prev = set(_children) 
-        self._op = _op 
+        self.grad = 0.0 # The derivative of the loss with respect to this value
+        self._backward = lambda: None # Closure that calculates the local gradient
+        self._prev = set(_children) # Set of nodes that created this node
+        self._op = _op # The operation that produced this node
         self.label = label
 
     def __repr__(self):
         return f"Value(data={self.data})"
 
     def __add__(self, other):
-        # 1. If 'other' is just a normal number (like 3), wrap it in a Value object so it can join the graph.
         other = other if isinstance(other, Value) else Value(other)
-        
-        # 2. Calculate the actual math (self.data + other.data).
-        # 3. Pass in the parents (self, other) to record where this came from.
-        # 4. Pass in the '+' string to record the operation.
         out = Value(self.data + other.data, (self, other), '+')
         return out
 
@@ -28,12 +25,33 @@ class Value:
         out = Value(self.data * other.data, (self, other), '*')
         return out
 
-    # The "Reverse" Dunder Methods
-    # What happens if we write `2 + Value(3)`? Python will try to call 2.__add__(Value(3)). 
-    # Since '2' is a native integer, it doesn't know what a 'Value' is and will crash.
-    # If standard __add__ fails, Python falls back to __radd__ (reverse add) on the second object.
     def __radd__(self, other): # other + self
         return self + other
 
     def __rmul__(self, other): # other * self
         return self * other
+
+    def __pow__(self, other):
+        assert isinstance(other, (int, float)), "only supporting int/float powers for now"
+        out = Value(self.data ** other, (self,), f'**{other}')
+        return out
+
+    def __neg__(self): # -self
+        return self * -1
+
+    def __sub__(self, other): # self - other
+        return self + (-other)
+
+    def __rsub__(self, other): # other - self
+        return other + (-self)
+
+    def __truediv__(self, other): # self / other
+        return self * (other ** -1)
+
+    def __rtruediv__(self, other): # other / self
+        return other * (self ** -1)
+
+    def exp(self):
+        x = self.data
+        out = Value(math.exp(x), (self,), 'exp')
+        return out
